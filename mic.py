0200d91c03d29f50643c79588f7498a86cdb9aa0
Kenny <kennyluvv66@gmail.com>
2024-02-12 19:38:39 +0300

    Speech recognizer

---
+++ mic.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr 
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+with mic as sourse:
+    r.adjust_for_ambient_noise(sourse)
+    audio = r.listen(sourse)
+
+print(r.recognize_google (audio))
