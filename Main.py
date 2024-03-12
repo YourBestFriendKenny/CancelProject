63babc7e6bfcae4eb0640904ccd30348a684c029
Kenny <kennyluvv66@gmail.com>
2024-03-12 18:33:28 +0300

    Исправил сохранение джисон
    Теперь добавлять музыку можно по одному которую выберет пользователь  и так же сохраняется в джисон и загружается

--- Main.py
+++ Main.py
@@ -35,7 +35,8 @@ class Player(QWidget, GuiMain.Ui_Form):
         self.sound_mixer = mixer
         self.sound_mixer.init()
         self.is_playing = False
-    
+
+        self.load_music_list()
 
     # проверка состояния музыки
     def toggle_sound(self):
@@ -60,6 +61,9 @@ class Player(QWidget, GuiMain.Ui_Form):
         if current_item:
             row = self.listWidget.currentRow()
             self.listWidget.takeItem(row)
+            self.sound_mixer.music.stop()
+            self.is_playing = False
+            self.pushbutton_play.setText("Play")
         
 
 
@@ -96,15 +100,49 @@ class Player(QWidget, GuiMain.Ui_Form):
                     file_name = os.path.splitext(os.path.basename(current_item))[0]
                     self.listWidget.addItem(file_name)
             self.dir = os.path.dirname(current_items[-1])
+            self.save_music_list()
 
 
     # регулировка громкости
     def volume_reg(self):
         self.sound_mixer.music.set_volume(self.horizontalSlider.value() / 100)
+
+    def load_music_list(self):
+        filename = "music_list.json"
+        if os.path.isfile(filename): 
+            try:
+                with open(filename, 'r') as f:
+                    data = json.load(f)
+                    for music_data in data:
+                        self.listWidget.addItem(music_data["filename"])
+            except FileNotFoundError:
+                print(f"Файл '{filename}' не найден.")
+            except Exception as e:
+                print(f"Произошла ошибка при загрузке списка музыки: {e}")
+
+    # Сохранение списка музыкальных файлов в файл JSON
+    def save_music_list(self):
+        filename = "music_list.json"
+        music_list = []
+        for index in range(self.listWidget.count()):
+            music_list.append({"filename": self.listWidget.item(index).text()})
+        try:
+            with open(filename, 'w') as f:
+                json.dump(music_list, f)
+        except Exception as e:
+            print(f"Произошла ошибка при сохранении списка музыки: {e}")
+
+    def remove_sound(self):
+        current_item = self.listWidget.currentItem()
+        if current_item:
+            row = self.listWidget.currentRow()
+            self.listWidget.takeItem(row)
+            self.sound_mixer.music.stop()
+            self.is_playing = False
+            self.pushbutton_play.setText("Play")
+    
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     player = Player()
     player.show()
     app.exec()
-
-
